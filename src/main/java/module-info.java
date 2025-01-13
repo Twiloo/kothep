ac4420f3ea82e8027f6d58bdf19cee6a40cd3e54
Andrea Grasso <andrea.grasso@eolas.fr>
2025-01-13 14:34:35 +0100

    PIZ-2 - Tests Mouvement et son

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module fr.twiloo.iut.kothep {
-    requires javafx.controls;
-    requires javafx.fxml;
-
     requires com.almasb.fxgl.all;
 
-    opens fr.twiloo.iut.kothep.client to javafx.fxml;
     exports fr.twiloo.iut.kothep.client;
+    opens assets.textures;
+    opens assets.sounds;
 }
